231ad1d621f5b19c3abb4b110ca8cccb52b13c8d
David Delahaye <delahaye3@hotmail.co.uk>
2020-12-04 01:29:17 +0000

    add page component

---
+++ components/Page.js
@@ -0,0 +1,18 @@
+import { NextSeo } from 'next-seo';
+
+const Page = ({ children }) => {
+  let path = '';
+  if (typeof window !== 'undefined') path = window.location.pathname;
+  const name = path.charAt(1).toUpperCase() + path.slice(2);
+  const title = `Fast Feedback - ${name}`;
+  console.log(title);
+  const url = `https://https://fastfeedback-blush.vercel.app${path}`;
+  return (
+    <>
+      <NextSeo title={title} canonical={url} openGraph={{ url, title }} />
+      {children}
+    </>
+  );
+};
+
+export default Page;
