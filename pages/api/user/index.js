85ec292a45a311161a2ac5518c3f53ef812323f5
david <delahaye3@hotmail.co.uk>
2020-12-23 23:33:50 +0000

    'addsitesCountToAccountPage'

---
+++ pages/api/user/index.js
@@ -0,0 +1,14 @@
+import { getUser } from '@/lib/db-admin';
+import { auth } from '@/lib/firebase-admin';
+
+export default async function handler(req, res) {
+    try {
+        const { uid } = await auth.verifyIdToken(req.headers.token);
+        console.log(uid);
+        console.log('here');
+        const { userDetails } = await getUser(uid);
+        res.status(200).json({ userDetails });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
