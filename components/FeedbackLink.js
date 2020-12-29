920f07db59da672bceb52df78b14451aead9f9c6
david <delahaye3@hotmail.co.uk>
2020-12-29 23:20:46 +0000

    'CleanupEmbedPage'

--- components/FeedbackLink.js
+++ components/FeedbackLink.js
@@ -6,13 +6,13 @@ export default function FeedbackLink({ slug }) {
   return (
     <Box mx="auto" w="full">
       <Flex justify="space-between" align="center">
-        <NextLink href={`/sites/${slug}`}>
+        <a href={`/sites/${slug}`} target='_blank'>
           <Link>
-            <Flex fontWeight="bold" align="center">
-              Leave a comment <ArrowRightIcon my="auto" />
+            <Flex fontWeight="bold" align="center" justifyContent='space-between'>
+              <Text mr='8px'>Leave a comment</Text> <ArrowRightIcon my="auto"/>
             </Flex>
           </Link>
-        </NextLink>
+        </a>
         <Text color="gray.400">Powered by FastFeedback</Text>
       </Flex>
     </Box>
